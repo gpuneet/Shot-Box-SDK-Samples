9a6589b0e51a6662c3dd19cf112cf51c39217984
sudheesh <Sudheesh@beesys.com>
2015-09-03 12:25:52 +0530

    Initial Commit

---
+++ GettingStarted/GettingStart.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace GettingStarted
+{
+    partial class GettingStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblServerIp = new System.Windows.Forms.Label();
+            this.lblSceneName = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtServerIp = new System.Windows.Forms.TextBox();
+            this.btnLoadScene = new System.Windows.Forms.Button();
+            this.btnFileDialog = new System.Windows.Forms.Button();
+            this.txtSceneName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnProgram = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblServerIp
+            // 
+            this.lblServerIp.AutoSize = true;
+            this.lblServerIp.Location = new System.Drawing.Point(20, 3);
+            this.lblServerIp.Name = "lblServerIp";
+            this.lblServerIp.Size = new System.Drawing.Size(47, 13);
+            this.lblServerIp.TabIndex = 0;
+            this.lblServerIp.Text = "ServerIp";
+            // 
+            // lblSceneName
+            // 
+            this.lblSceneName.AutoSize = true;
+            this.lblSceneName.Location = new System.Drawing.Point(20, 5);
+            this.lblSceneName.Name = "lblSceneName";
+            this.lblSceneName.Size = new System.Drawing.Size(70, 13);
+            this.lblSceneName.TabIndex = 1;
+            this.lblSceneName.Text = "Scene Graph";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConnect.Location = new System.Drawing.Point(312, 16);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(72, 25);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtServerIp
+            // 
+            this.txtServerIp.Location = new System.Drawing.Point(110, 0);
+            this.txtServerIp.Name = "txtServerIp";
+            this.txtServerIp.Size = new System.Drawing.Size(164, 20);
+            this.txtServerIp.TabIndex = 3;
+            // 
+            // btnLoadScene
+            // 
+            this.btnLoadScene.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoadScene.Location = new System.Drawing.Point(392, 16);
+            this.btnLoadScene.Name = "btnLoadScene";
+            this.btnLoadScene.Size = new System.Drawing.Size(92, 36);
+            this.btnLoadScene.TabIndex = 4;
+            this.btnLoadScene.Text = "LoadScene";
+            this.btnLoadScene.UseVisualStyleBackColor = true;
+            this.btnLoadScene.Click += new System.EventHandler(this.btnLoadScene_Click);
+            // 
+            // btnFileDialog
+            // 
+            this.btnFileDialog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFileDialog.Location = new System.Drawing.Point(312, 44);
+            this.btnFileDialog.Name = "btnFileDialog";
+            this.btnFileDialog.Size = new System.Drawing.Size(72, 26);
+            this.btnFileDialog.TabIndex = 6;
+            this.btnFileDialog.Text = "...";
+            this.btnFileDialog.UseVisualStyleBackColor = true;
+            this.btnFileDialog.Click += new System.EventHandler(this.btnFileDialog_Click);
+            // 
+            // txtSceneName
+            // 
+            this.txtSceneName.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txtSceneName.Location = new System.Drawing.Point(110, 0);
+            this.txtSceneName.Name = "txtSceneName";
+            this.txtSceneName.Size = new System.Drawing.Size(167, 20);
+            this.txtSceneName.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStop);
+            this.groupBox2.Controls.Add(this.btnPause);
+            this.groupBox2.Controls.Add(this.btnPlay);
+            this.groupBox2.Location = new System.Drawing.Point(12, 83);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 150);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controller";
+            // 
+            // btnStop
+            // 
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Location = new System.Drawing.Point(6, 104);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(161, 27);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPause.Location = new System.Drawing.Point(6, 62);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(161, 27);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Location = new System.Drawing.Point(6, 19);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(161, 28);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPreview.Location = new System.Drawing.Point(12, 62);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(153, 29);
+            this.btnPreview.TabIndex = 9;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnProgram
+            // 
+            this.btnProgram.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProgram.Location = new System.Drawing.Point(10, 19);
+            this.btnProgram.Name = "btnProgram";
+            this.btnProgram.Size = new System.Drawing.Size(155, 28);
+            this.btnProgram.TabIndex = 8;
+            this.btnProgram.Text = "Program";
+            this.btnProgram.UseVisualStyleBackColor = true;
+            this.btnProgram.Click += new System.EventHandler(this.btnProgram_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblServerIp);
+            this.panel1.Controls.Add(this.txtServerIp);
+            this.panel1.Location = new System.Drawing.Point(12, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 22);
+            this.panel1.TabIndex = 81;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblSceneName);
+            this.panel2.Controls.Add(this.txtSceneName);
+            this.panel2.Location = new System.Drawing.Point(12, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(277, 22);
+            this.panel2.TabIndex = 82;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnProgram);
+            this.groupBox1.Controls.Add(this.btnPreview);
+            this.groupBox1.Location = new System.Drawing.Point(211, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 150);
+            this.groupBox1.TabIndex = 83;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Render Mode";
+            // 
+            // GettingStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 237);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnFileDialog);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnLoadScene);
+            this.Name = "GettingStart";
+            this.Text = "GettingStarted";
+            this.Load += new System.EventHandler(this.GettingStart_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblServerIp;
+        private System.Windows.Forms.Label lblSceneName;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtServerIp;
+        private System.Windows.Forms.Button btnLoadScene;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox txtSceneName;
+        private System.Windows.Forms.Button btnFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnProgram;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ GettingStarted/GettingStart.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Beesys.Wasp.Workflow;
+using System.Configuration;
+using System.IO;
+using System.Collections;
+
+namespace GettingStarted
+{
+    public partial class GettingStart : Form
+    {
+
+        #region class members
+
+        private              ShotBox                      m_objShotBox = null;
+        private              Link                         m_objLink = null;
+        private              LinkManager                  m_objLinkManager = null;
+        private              string                       m_sServerIp = string.Empty;
+        private              string                       m_sUrl = "net.tcp://{0}:{1}/TcpBinding/WcfTcpLink";
+        private              bool                         m_isPause = false;
+        private              FileInfo                     m_objFileInfo = null;
+        private              string                       m_sLinkType = string.Empty;
+        private              string                       m_sPort = string.Empty;
+        private              const string                 MODULENAME = "WASP";
+
+        #endregion
+
+
+        #region constructor
+
+        public GettingStart()
+        {
+            InitializeComponent();
+        }//end (GettingStart)
+
+        #endregion
+
+
+        #region Events
+
+        /// <summary>
+        /// used to connect with the server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                if (Equals(m_objShotBox, null) && !string.IsNullOrEmpty(txtServerIp.Text))
+                {
+                    switch (m_sLinkType.ToLower())
+                    {
+                        case "namedpipe":
+                            m_sServerIp = string.Format("net.pipe://{0}/WcfNamedPipeLink", txtServerIp.Text);
+                            break;
+                        default:
+                            m_sServerIp = string.Format(m_sUrl, txtServerIp.Text, m_sPort);
+                            break;
+                    }//end (switch)
+                    m_objLink.Connect(m_sServerIp);
+                }//end (if)
+
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+
+        }//end (btnConnect_Click)
+
+
+        /// <summary>
+        /// fires when engine is connected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void objLink_OnEngineConnected(object sender, EngineArgs e)
+        {
+            btnConnect.BackColor = Color.DarkGreen;
+        }//end (objLink_OnEngineConnected)
+
+
+        /// <summary>
+        /// this event fires when user wants to close the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void GettingStart_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (!Equals(m_objShotBox, null))
+                    m_objShotBox.DeleteSg();
+                if (!Equals(m_objLink, null))
+                    m_objLink.DisconnectAll();
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+        }//end (GettingStart_FormClosing)
+
+
+        /// <summary>
+        /// used to selecting the scenegraph
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFileDialog_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "wsl or w3d files (*.wsl;*.w3d)|*.wsl;*.w3d";
+                openFileDialog1.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                if (Equals(openFileDialog1.ShowDialog(), DialogResult.OK))
+                {
+                    m_objFileInfo = new FileInfo(openFileDialog1.FileName);
+                    txtSceneName.Text = m_objFileInfo.Name;
+                }//end (if)
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+        }//end (btnFileDialog_Click)
+
+
+        /// <summary>
+        /// used to setting the mode as program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnProgram_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Equals(m_objShotBox, null))
+                {
+                    m_objShotBox.SetMode(RENDERMODE.PROGRAM);
+                    btnProgram.BackColor = Color.DarkGray;
+                    btnPreview.BackColor = Color.DarkGreen;
+                }//end (if)
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+
+        }//end (btnProgram_Click)
+
+
+        /// <summary>
+        /// used to setting the mode as preview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Equals(m_objShotBox, null))
+                {
+                    m_objShotBox.SetMode(RENDERMODE.PREVIEW);
+                    btnPreview.BackColor = Color.DarkGray;
+                    btnProgram.BackColor = Color.DarkGreen;
+                }//end (if)
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+
+        }//end (btnPreview_Click)
+
+
+        /// <summary>
+        /// used to pause the scenegraph
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Equals(m_objShotBox, null))
+                {
+                    btnPause.BackColor = Color.DarkGray;
+                    m_objShotBox.Pause();
+                    m_isPause = true;
+                }//end (if)
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+
+        }//end (btnPause_Click)
+
+
+        /// <summary>
+        /// used to stop the scenegraph
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Equals(m_objShotBox, null))
+                {
+                    btnStop.BackColor = Color.DarkGray;
+                    m_objShotBox.Stop();
+                    m_isPause = false;
+                }//end (if)
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+
+        }//end (btnStop_Click)
+
+
+        /// <summary>
+        /// used to play the scenegraph
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Equals(m_objShotBox, null))
+                {
+                    btnPlay.BackColor = Color.DarkGray;
+                    if (!m_isPause)
+                    {
+                        m_objShotBox.Play(true, true);
+                    }//end (if)
+                    else
+                        m_objShotBox.Play(false, false);
+                    m_isPause = false;
+                }//end (if)
+
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+
+
+        }//end (btnPlay_Click)
+
+
+        /// <summary>
+        /// used for preparing the scenegraph
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLoadScene_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                #region sceneone
+                string sSgxml = Util.getSGFromWSL(openFileDialog1.FileName);
+                string shotBoxID = null;
+                bool isTicker;
+                string filetype = string.Empty;
+
+                if (!string.IsNullOrEmpty(sSgxml))
+                {
+
+                    m_objShotBox = m_objLink.GetShotBox(sSgxml, out shotBoxID, out isTicker) as ShotBox;
+                    filetype = Path.GetExtension(openFileDialog1.FileName).Split(new string[] { "." }, StringSplitOptions.None)[1];
+                    if (!Equals(m_objShotBox, null))
+                    {
+                        m_objShotBox.SetEngineUrl(m_sServerIp);
+                        if (m_objShotBox is IAddinInfo)
+                            (m_objShotBox as IAddinInfo).Init(new InstanceInfo() 
+                            {
+                                Type = filetype, 
+                                InstanceId = string.Empty, 
+                                TemplateId = openFileDialog1.FileName, 
+                                ThemeId = "default",
+                                 IsPreview=false
+                            });
+                        m_objShotBox.OnShotBoxStatus += new EventHandler<SHOTBOXARGS>(m_objShotBox_OnShotBoxStatus);
+                        m_objShotBox.Prepare(m_sServerIp, 0, RENDERMODE.PROGRAM);
+
+                    }//end (if)
+
+                }//end (if)
+                #endregion
+
+
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+
+
+        }//end (btnLoadScene_Click)
+
+        /// <summary>
+        /// this event fires when scenegraph is loaded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_objShotBox_OnShotBoxStatus(object sender, SHOTBOXARGS e)
+        {
+            if (Equals(e.SHOTBOXRESPONSE, SHOTBOXMSG.PREPARED))
+            {
+                btnPlay.BackColor = Color.DarkGreen;
+                btnProgram.BackColor = Color.DarkGreen;
+            }//end (if)
+        }//end (m_objShotBox_OnShotBoxStatus)
+
+
+        /// <summary>
+        /// used to getting the link
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GettingStart_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string sLinkID = null;
+
+                m_sPort = ConfigurationManager.AppSettings["port"].ToString();
+                txtServerIp.Text = ConfigurationManager.AppSettings["ipconfig"].ToString();
+                m_sLinkType = ConfigurationManager.AppSettings["linktype"].ToString();
+                m_objLinkManager = new LinkManager();
+                if (!Equals(m_objLinkManager, null))
+                {
+                    if (string.Compare(m_sLinkType, "TCP", StringComparison.OrdinalIgnoreCase) == 0)
+                        m_objLink = m_objLinkManager.GetLink(LINKTYPE.TCP, out sLinkID);
+                    if (string.Compare(m_sLinkType, "NAMEDPIPE", StringComparison.OrdinalIgnoreCase) == 0)
+                        m_objLink = m_objLinkManager.GetLink(LINKTYPE.NAMEDPIPE, out sLinkID);
+
+                }//end (if)
+
+                if (!Equals(m_objLink, null))
+                    m_objLink.OnEngineConnected += new EventHandler<EngineArgs>(objLink_OnEngineConnected);
+
+                this.FormClosing += new FormClosingEventHandler(GettingStart_FormClosing);
+
+            }//end (try)
+            catch (Exception ex)
+            {
+                LogWriter.WriteLog(MODULENAME, ex);
+            }//end (catch)
+        }//end (GettingStart_Load)
+
+        #endregion
+
+
+     
+    }
+}
